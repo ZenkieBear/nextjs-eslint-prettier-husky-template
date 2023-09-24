21a78c14c0ce5222395627a50cafe3ab389f222e
Zenkie Bear <zq@zenkie.cn>
2023-09-24 23:00:39 +0800

    test github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from 'react'
 
 export default function Home() {
+  const [a, setA] = useState(0);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
